89eebe5a4bc87e1e6776fda5a1e62a69aa967ee7
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-04-22 14:18:24 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/campmarket/CampMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.campmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/campmarket/CampMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
